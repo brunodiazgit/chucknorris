91fa2c553cdb2e16ff0baa6a7626db1a1246621f
Bruno Díaz <brunodiaznt@gmail.com>
2023-09-05 20:31:26 -0300

    cambios

--- norris/index.js
+++ norris/index.js
@@ -1,9 +1,12 @@
 const btn = document.getElementById("btn");
+const frasesContainer = document.getElementById("frases");
 
 btn.addEventListener("click", ()=>{
-    
+ 
 fetch("https://api.chucknorris.io/jokes/random")
 .then(response => response.json())
-.then(data => console.log(data))
-
+.then(data => {
+    // Aquí mostramos la frase en el elemento "frases" del HTML
+    frasesContainer.innerHTML = data.value;
+})
 });
