1e5ce24479a130b88204d422ac463b938a23197a
Bruno Díaz <brunodiaznt@gmail.com>
2023-09-05 20:37:55 -0300

    s

--- norris/index.js
+++ norris/index.js
@@ -6,7 +6,6 @@ btn.addEventListener("click", ()=>{
 fetch("https://api.chucknorris.io/jokes/random")
 .then(response => response.json())
 .then(data => {
-    // Aquí mostramos la frase en el elemento "frases" del HTML
     frasesContainer.innerHTML = data.value;
 })
 });
